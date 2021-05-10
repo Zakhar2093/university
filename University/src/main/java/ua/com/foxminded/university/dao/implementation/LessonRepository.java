2f4cf2b484ee4d86cc2ba21177b84a57d8848504
Zakhar Khruschov <zygresnik@yandex.ua>
2021-05-10 14:14:15 +0300

    Created StudentRepository instead of StudentDaoImpl. Added tests to StudentRepository

---
+++ University/src/main/java/ua/com/foxminded/university/dao/implementation/LessonRepository.java
@@ -0,0 +1,253 @@
+package ua.com.foxminded.university.dao.implementation;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import ua.com.foxminded.university.dao.interfaces.LessonDao;
+import ua.com.foxminded.university.dao.interfaces.StudentDao;
+import ua.com.foxminded.university.dao.interfaces.TeacherDao;
+import ua.com.foxminded.university.exception.DaoException;
+import ua.com.foxminded.university.model.Group;
+import ua.com.foxminded.university.model.Lesson;
+import ua.com.foxminded.university.model.Teacher;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Repository
+public class LessonRepository implements LessonDao{
+
+    private static final Logger logger = LoggerFactory.getLogger(LessonRepository.class);
+    private SessionFactory sessionFactory;
+    private TeacherDao teacherDao;
+    private StudentDao studentDao;
+
+    @Autowired
+    public LessonRepository(SessionFactory sessionFactory, TeacherDao teacherDao, StudentDao studentDao) {
+        this.sessionFactory = sessionFactory;
+        this.teacherDao = teacherDao;
+        this.studentDao = studentDao;
+    }
+
+    public void create(Lesson lesson) {
+        logger.debug("Creating lesson with name {} {}", lesson.getLessonName());
+        try (Session session = sessionFactory.openSession()){
+            session.save(lesson);
+        } catch (PersistenceException e) {
+            logger.error("Creating was not successful. Lesson can not be created. Some field is null", e);
+            throw new DaoException("Lesson can not be created. Some field is null", e);
+        }
+        logger.debug("Creating was successful");
+    }
+
+    public List<Lesson> getAll() {
+        logger.debug("Getting all Lessons");
+        Session session = sessionFactory.openSession();
+        Query query = session.createQuery("from Lesson");
+        return query.getResultList();
+    }
+
+    public Lesson getById(Integer lessonId) {
+        logger.debug("Getting lesson by id = {}", lessonId);
+        Session session = sessionFactory.openSession();
+        Lesson lesson = Optional.ofNullable(session.get(Lesson.class, lessonId))
+                .orElseThrow(() -> new DaoException(String.format("Lesson with such id %d does not exist", lessonId)))
+                ;
+        return lesson;
+    }
+
+    public void update(Lesson lesson) {
+        logger.debug("Updating lesson with name {} {}", lesson.getLessonName());
+        try (Session session = sessionFactory.openSession()){
+            Transaction tx = session.beginTransaction();
+
+            session.merge(lesson);
+
+            tx.commit();
+        } catch (PersistenceException e) {
+            logger.error("Updating was not successful. Lesson can not be updated. Some new field is null", e);
+            throw new DaoException("Lesson can not be updated. Some new field is null", e);
+        }
+        logger.debug("Updating was successful");
+    }
+
+    public void deactivate(Integer lessonId) {
+        logger.debug("Deactivating lesson with id = {}", lessonId);
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("UPDATE Lesson L SET L.lessonInactive = true, L.group = null, L.room = null, L.teacher = null WHERE L.lessonId =: lessonId");
+        query.setParameter("lessonId", lessonId);
+        query.executeUpdate();
+
+        tx.commit();
+        session.close();
+        logger.debug("Deactivating was successful", lessonId);
+    }
+
+    public void activate(Integer lessonId) {
+        logger.debug("Activating lesson with id = {}", lessonId);
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("UPDATE Lesson SET lessonInactive = false WHERE id =: lessonId");
+        query.setParameter("lessonId", lessonId);
+        query.executeUpdate();
+
+        tx.commit();
+        session.close();
+        logger.debug("Activating was successful", lessonId);
+    }
+
+
+    @Override
+    public List<Lesson> getLessonByTeacherIdForDay(int teacherId, LocalDateTime date) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("FROM Lesson L WHERE L.teacher =: teacher AND EXTRACT(YEAR FROM L.date) =: year AND EXTRACT(MONTH FROM L.date) =: month AND EXTRACT(DAY FROM L.date) =: day");
+        query.setParameter("teacher", teacherDao.getById(teacherId));
+        query.setParameter("year", date.getYear());
+        query.setParameter("month", date.getMonthValue());
+        query.setParameter("day", date.getDayOfMonth());
+        List<Lesson> lessons = query.getResultList();
+
+        tx.commit();
+        session.close();
+        return lessons;
+    }
+
+    @Override
+    public List<Lesson> getLessonByTeacherIdForMonth(int teacherId, LocalDateTime date) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("FROM Lesson L WHERE L.teacher =: teacher AND EXTRACT(YEAR FROM L.date) =: year AND EXTRACT(MONTH FROM L.date) =: month");
+        query.setParameter("teacher", teacherDao.getById(teacherId));
+        query.setParameter("year", date.getYear());
+        query.setParameter("month", date.getMonthValue());
+        List<Lesson> lessons = query.getResultList();
+
+        tx.commit();
+        session.close();
+        return lessons;
+    }
+
+    @Override
+    public List<Lesson> getLessonByStudentIdForDay(int studentId, LocalDateTime date) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("FROM Lesson L WHERE L.group =: group AND EXTRACT(YEAR FROM L.date) =: year AND EXTRACT(MONTH FROM L.date) =: month AND EXTRACT(DAY FROM L.date) =: day");
+        query.setParameter("group", studentDao.getById(studentId).getGroup());
+        query.setParameter("year", date.getYear());
+        query.setParameter("month", date.getMonthValue());
+        query.setParameter("day", date.getDayOfMonth());
+        List<Lesson> lessons = query.getResultList();
+
+        tx.commit();
+        session.close();
+        return lessons;
+    }
+
+    @Override
+    public List<Lesson> getLessonByStudentIdForMonth(int studentId, LocalDateTime date) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("FROM Lesson L WHERE L.group =: group AND EXTRACT(YEAR FROM L.date) =: year AND EXTRACT(MONTH FROM L.date) =: month");
+        query.setParameter("group", studentDao.getById(studentId).getGroup());
+        query.setParameter("year", date.getYear());
+        query.setParameter("month", date.getMonthValue());
+        List<Lesson> lessons = query.getResultList();
+
+        tx.commit();
+        session.close();
+        return lessons;
+    }
+
+    @Override
+    public void addGroupToLesson(Integer groupId, Integer lessonId) {
+
+    }
+    // todo private
+// todo common method
+    @Override
+    public void removeGroupFromLesson(Integer lessonId) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("UPDATE Lesson L SET L.group = null WHERE L.lessonId =: lessonId");
+        query.setParameter("lessonId", lessonId);
+        query.executeUpdate();
+
+        tx.commit();
+        session.close();
+
+        logger.debug("removed Group from group Lesson with id = {} ", lessonId);
+    }
+
+    @Override
+    public void removeRoomFromLesson(Integer lessonId) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("UPDATE Lesson L SET L.room = null WHERE L.lessonId =: lessonId");
+        query.setParameter("lessonId", lessonId);
+        query.executeUpdate();
+
+        tx.commit();
+        session.close();
+
+        logger.debug("removed Room from group Lesson with id = {} ", lessonId);
+    }
+
+    @Override
+    public void removeTeacherFromLesson(Integer lessonId) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Query query = session.createQuery("UPDATE Lesson L SET L.teacher = null WHERE L.lessonId =: lessonId");
+        query.setParameter("lessonId", lessonId);
+        query.executeUpdate();
+
+        tx.commit();
+        session.close();
+
+        logger.debug("removed Teacher from group Lesson with id = {} ", lessonId);
+    }
+
+    @Override
+    public void addTeacherToLesson(Integer teacherId, Integer lessonId) {
+
+    }
+
+    @Override
+    public void addRoomToLesson(Integer roomId, Integer lessonId) {
+
+    }
+
+    @Override
+    public List<Lesson> getLessonsByGroupId(Integer groupId) {
+        return null;
+    }
+
+    @Override
+    public List<Lesson> getLessonsByRoomId(Integer groupId) {
+        return null;
+    }
+
+    @Override
+    public List<Lesson> getLessonsByTeacherId(Integer groupId) {
+        return null;
+    }
+}
---
+++ University/src/test/java/ua/com/foxminded/university/dao/hibernate/LessonRepositoryTest.java
@@ -0,0 +1,283 @@
+package ua.com.foxminded.university.dao.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import ua.com.foxminded.university.SpringConfigTest;
+import ua.com.foxminded.university.dao.interfaces.*;
+import ua.com.foxminded.university.exception.DaoException;
+import ua.com.foxminded.university.model.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = SpringConfigTest.class)
+@WebAppConfiguration
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class LessonRepositoryTest {
+
+    private static final String FORMAT = "yyyy.MM.dd-HH.mm.ss";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(FORMAT);
+
+    @Autowired
+    private GroupDao groupDao;
+    @Autowired
+    private LessonDao lessonDao;
+    @Autowired
+    private TeacherDao teacherDao;
+    @Autowired
+    private RoomDao roomDao;
+    @Autowired
+    private StudentDao studentDao;
+
+    @Test
+    void getByIdAndCreateShouldInsertAndGetCorrectData() {
+        Lesson expected = createLesson();
+        Lesson actual = lessonDao.getById(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAllAndCreateShouldInsertAndGetCorrectData() {
+        Group group = new Group(1, "any", false);
+        groupDao.create(group);
+        Room room = new Room(1, 1, false);
+        roomDao.create(room);
+        Teacher teacher = new Teacher(1, "one", "one", false);
+        teacherDao.create(teacher);
+
+
+        List<Lesson> lessons = new ArrayList<>();
+        lessons.add(new Lesson(1, "one", teacher, group, room, LocalDateTime.now(), false));
+        lessons.add(new Lesson(2, "two", teacher, group, room, LocalDateTime.now(), false));
+        lessons.add(new Lesson(3, "three", teacher, group, room, LocalDateTime.now(), false));
+        lessonDao.create(lessons.get(0));
+        lessonDao.create(lessons.get(1));
+        lessonDao.create(lessons.get(2));
+
+        List<Lesson> expected = lessons;
+        List<Lesson> actual = lessonDao.getAll();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void updateShouldUpdateCorrectData() {
+        Group group = new Group(1, "any", false);
+        groupDao.create(group);
+        Room room = new Room(1, 1, false);
+        roomDao.create(room);
+        Teacher teacher = new Teacher(1, "one", "one", false);
+        teacherDao.create(teacher);
+
+        Lesson groupBeforeUpdating = new Lesson(1, "one", teacher, group, room, LocalDateTime.now(), false);
+        Lesson groupAfterUpdating = new Lesson(1, "one", teacher, group, room, LocalDateTime.now(), false);
+        lessonDao.create(groupBeforeUpdating);
+        lessonDao.update(groupAfterUpdating);
+        Lesson expected = groupAfterUpdating;
+        Lesson actual = lessonDao.getById(1);
+        List<Lesson> groups = lessonDao.getAll();
+
+        assertTrue(groups.size() == 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void deactivateShouldSetTrueInLessonInactive() {
+        Lesson lesson = createLesson();
+        lessonDao.deactivate(lesson.getLessonId());
+        assertTrue(lessonDao.getById(lesson.getLessonId()).isLessonInactive());
+        assertTrue(teacherDao.getById(1).getLessons().isEmpty());
+        assertTrue(roomDao.getById(1).getLessons().isEmpty());
+        assertTrue(groupDao.getById(1).getLessons().isEmpty());
+    }
+
+    @Test
+    void activateShouldSetFalseInLessonInactive() {
+        Lesson lesson = createLesson();
+        lessonDao.deactivate(lesson.getLessonId());
+        assertTrue(lessonDao.getById(lesson.getLessonId()).isLessonInactive());
+        lessonDao.activate(1);
+        assertFalse(lessonDao.getById(lesson.getLessonId()).isLessonInactive());
+    }
+
+    @Test
+    void whenGetByIdGetNonexistentDataShouldThrowsDaoException() {
+        DaoException thrown = assertThrows(DaoException.class, () -> {
+            lessonDao.getById(1);
+        });
+        assertTrue(thrown.getMessage().contains("Lesson with such id 1 does not exist"));
+    }
+
+    @Test
+    void getLessonByTeacherForDayShouldReturnCorrectData() {
+        Group group = new Group(1, "any name", false);
+        groupDao.create(group);
+        Teacher teacher = new Teacher(1, "one", "one", false);
+        Teacher teacher2 = new Teacher(2, "two", "two", false);
+        teacherDao.create(teacher);
+        teacherDao.create(teacher2);
+        Room room = new Room(1, 101);
+        roomDao.create(room);
+
+        LocalDateTime date1 = LocalDateTime.parse("2021.01.20-23.55.11", FORMATTER);
+        LocalDateTime date2 = LocalDateTime.parse("2021.01.21-03.00.00", FORMATTER);
+        LocalDateTime date3 = LocalDateTime.parse("2021.01.21-23.55.11", FORMATTER);
+        LocalDateTime date4 = LocalDateTime.parse("2021.02.11-23.55.11", FORMATTER);
+
+        Lesson lesson1 = new Lesson(1, "Math", teacher, group, room, date1, false);
+        Lesson lesson2 = new Lesson(2, "bio", teacher, group, room, date2, false);
+        Lesson lesson3 = new Lesson(3, "bio", teacher2, group, room, date2, false);
+        Lesson lesson4 = new Lesson(4, "history", teacher, group, room, date3, false);
+        Lesson lesson5 = new Lesson(5, "english", teacher, group, room, date4, false);
+
+        lessonDao.create(lesson1);
+        lessonDao.create(lesson2);
+        lessonDao.create(lesson3);
+        lessonDao.create(lesson4);
+        lessonDao.create(lesson5);
+
+        List<Lesson> actual = lessonDao.getLessonByTeacherIdForDay(teacher.getTeacherId(), date2);
+        List<Lesson> expected = new ArrayList<>();
+        expected.add(lesson2);
+        expected.add(lesson4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getLessonByTeacherForMonthShouldReturnCorrectData() {
+        Group group = new Group(1, "any name", false);
+        groupDao.create(group);
+        Teacher teacher = new Teacher(1, "one", "one", false);
+        Teacher teacher2 = new Teacher(2, "two", "two", false);
+        teacherDao.create(teacher);
+        teacherDao.create(teacher2);
+        Room room = new Room(1, 101);
+        roomDao.create(room);
+
+        LocalDateTime date1 = LocalDateTime.parse("2021.01.20-23.55.11", FORMATTER);
+        LocalDateTime date2 = LocalDateTime.parse("2021.01.21-03.00.00", FORMATTER);
+        LocalDateTime date3 = LocalDateTime.parse("2021.01.21-23.55.11", FORMATTER);
+        LocalDateTime date4 = LocalDateTime.parse("2021.02.11-23.55.11", FORMATTER);
+
+        Lesson lesson1 = new Lesson(1, "Math", teacher, group, room, date1, false);
+        Lesson lesson2 = new Lesson(2, "bio", teacher, group, room, date2, false);
+        Lesson lesson3 = new Lesson(3, "bio", teacher2, group, room, date2, false);
+        Lesson lesson4 = new Lesson(4, "history", teacher, group, room, date3, false);
+        Lesson lesson5 = new Lesson(5, "english", teacher, group, room, date4, false);
+
+        lessonDao.create(lesson1);
+        lessonDao.create(lesson2);
+        lessonDao.create(lesson3);
+        lessonDao.create(lesson4);
+        lessonDao.create(lesson5);
+
+        List<Lesson> actual = lessonDao.getLessonByTeacherIdForMonth(teacher.getTeacherId(), date2);
+        List<Lesson> expected = new ArrayList<>();
+        expected.add(lesson1);
+        expected.add(lesson2);
+        expected.add(lesson4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getLessonByStudentForDayShouldReturnCorrectData() {
+        Group group = new Group(1, "any name", false);
+        groupDao.create(group);
+        Teacher teacher = new Teacher(1, "one", "one", false);
+        Teacher teacher2 = new Teacher(2, "two", "two", false);
+        teacherDao.create(teacher);
+        teacherDao.create(teacher2);
+        Room room = new Room(1, 101);
+        roomDao.create(room);
+        Student student1 = new Student(1, "student1", "student1", group, false);
+        Student student2 = new Student(2, "student2", "student2", group, false);
+        studentDao.create(student1);
+        studentDao.create(student2);
+
+        LocalDateTime date1 = LocalDateTime.parse("2021.01.20-23.55.11", FORMATTER);
+        LocalDateTime date2 = LocalDateTime.parse("2021.01.21-03.00.00", FORMATTER);
+        LocalDateTime date3 = LocalDateTime.parse("2021.01.21-23.55.11", FORMATTER);
+        LocalDateTime date4 = LocalDateTime.parse("2021.02.11-23.55.11", FORMATTER);
+
+        Lesson lesson1 = new Lesson(1, "Math", teacher, group, room, date1, false);
+        Lesson lesson2 = new Lesson(2, "bio", teacher, group, room, date2, false);
+        Lesson lesson3 = new Lesson(3, "history", teacher, group, room, date3, false);
+        Lesson lesson4 = new Lesson(4, "english", teacher, group, room, date4, false);
+
+        lessonDao.create(lesson1);
+        lessonDao.create(lesson2);
+        lessonDao.create(lesson3);
+        lessonDao.create(lesson4);
+
+        List<Lesson> actual = lessonDao.getLessonByStudentIdForDay(student1.getStudentId(), date2);
+        List<Lesson> expected = new ArrayList<>();
+        expected.add(lesson2);
+        expected.add(lesson3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getLessonByStudentForMonthShouldReturnCorrectData() {
+        Group group = new Group(1, "any name", false);
+        groupDao.create(group);
+        Teacher teacher = new Teacher(1, "one", "one", false);
+        Teacher teacher2 = new Teacher(2, "two", "two", false);
+        teacherDao.create(teacher);
+        teacherDao.create(teacher2);
+        Room room = new Room(1, 101);
+        roomDao.create(room);
+        Student student1 = new Student(1, "student1", "student1", group, false);
+        Student student2 = new Student(2, "student2", "student2", group, false);
+        studentDao.create(student1);
+        studentDao.create(student2);
+
+        LocalDateTime date1 = LocalDateTime.parse("2021.01.20-23.55.11", FORMATTER);
+        LocalDateTime date2 = LocalDateTime.parse("2021.01.21-03.00.00", FORMATTER);
+        LocalDateTime date3 = LocalDateTime.parse("2021.01.21-23.55.11", FORMATTER);
+        LocalDateTime date4 = LocalDateTime.parse("2021.02.11-23.55.11", FORMATTER);
+
+        Lesson lesson1 = new Lesson(1, "Math", teacher, group, room, date1, false);
+        Lesson lesson2 = new Lesson(2, "bio", teacher, group, room, date2, false);
+        Lesson lesson3 = new Lesson(3, "history", teacher, group, room, date3, false);
+        Lesson lesson4 = new Lesson(4, "english", teacher, group, room, date4, false);
+
+        lessonDao.create(lesson1);
+        lessonDao.create(lesson2);
+        lessonDao.create(lesson3);
+        lessonDao.create(lesson4);
+
+        List<Lesson> actual = lessonDao.getLessonByStudentIdForMonth(student1.getStudentId(), date2);
+        List<Lesson> expected = new ArrayList<>();
+        expected.add(lesson1);
+        expected.add(lesson2);
+        expected.add(lesson3);
+
+        assertEquals(expected, actual);
+    }
+
+    private Lesson createLesson() {
+        Group group = new Group(1, "any", false);
+        groupDao.create(group);
+        Room room = new Room(1, 1, false);
+        roomDao.create(room);
+        Teacher teacher = new Teacher(1, "one", "one", false);
+        teacherDao.create(teacher);
+        Lesson lesson = new Lesson(1, "one", teacher, group, room, LocalDateTime.now(), false);
+        lessonDao.create(lesson);
+        return lesson;
+    }
+}
