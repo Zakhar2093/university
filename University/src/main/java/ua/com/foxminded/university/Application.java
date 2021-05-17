2feb248222b938407b3068e8a17350144b3df3e0
Zakhar Khruschov <zygresnik@yandex.ua>
2021-05-18 02:08:26 +0300

    Added JNDI

--- University/src/main/java/ua/com/foxminded/university/Application.java
+++ University/src/main/java/ua/com/foxminded/university/Application.java
@@ -1,13 +1,19 @@
 package ua.com.foxminded.university;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ua.com.foxminded.university.service.RoomService;
+
+import javax.sql.DataSource;
 
 @EnableJpaRepositories("ua/com/foxminded/university/repository")
 @EntityScan("ua/com/foxminded/university/model")
@@ -15,5 +21,28 @@ import ua.com.foxminded.university.service.RoomService;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer(Environment env) {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected TomcatWebServer getTomcatWebServer(Tomcat tomcat) {
+                tomcat.enableNaming();
+                return super.getTomcatWebServer(tomcat);
+            }
+            @Override
+            protected void postProcessContext(Context context) {
+                ContextResource resource = new ContextResource();
+                resource.setName("/jdbc/postgres");
+                resource.setType(DataSource.class.getName());
+                resource.setProperty("url", env.getProperty("datasource.url"));
+                resource.setProperty("username", env.getProperty("datasource.username"));
+                resource.setProperty("password",env.getProperty("datasource.password"));
+                context.getNamingResources().addResource(resource);
+                super.postProcessContext(context);
+            }
+        };
+        return tomcat;
     }
 }
