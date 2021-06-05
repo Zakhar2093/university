4cf5d2bc809dd520eb80684ef931ae7ef8439188
Zakhar <zygresnik@yandex.ua>
2021-06-05 13:17:24 +0300

    Added @Transactional to RoomRepository and TeacherRepository otherwise in invoke javax.persistence.TransactionRequiredException: Executing an update/delete query

--- src/main/java/ua/com/foxminded/university/repository/RoomRepository.java
+++ src/main/java/ua/com/foxminded/university/repository/RoomRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import ua.com.foxminded.university.model.Room;
 
+import javax.transaction.Transactional;
+
 public interface RoomRepository extends JpaRepository<Room, Integer> {
 
+    @Transactional
     @Modifying
     @Query("UPDATE Lesson L SET L.room = null WHERE L.room.roomId = :roomId")
     void removeRoomFromAllLessons(Integer roomId);
--- src/main/java/ua/com/foxminded/university/repository/TeacherRepository.java
+++ src/main/java/ua/com/foxminded/university/repository/TeacherRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import ua.com.foxminded.university.model.Teacher;
 
+import javax.transaction.Transactional;
+
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
 
+    @Transactional
     @Modifying
     @Query("UPDATE Lesson L SET L.teacher = null WHERE L.teacher.teacherId = :teacherId")
     void removeTeacherFromAllLessons(Integer teacherId);
