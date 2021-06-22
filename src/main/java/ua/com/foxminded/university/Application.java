028a7ae3643009bec9f82d4085231997085e4bc4
Zakhar <zygresnik@yandex.ua>
2021-06-22 12:39:35 +0300

    Added Swagger support

--- src/main/java/ua/com/foxminded/university/Application.java
+++ src/main/java/ua/com/foxminded/university/Application.java
@@ -3,7 +3,12 @@ package ua.com.foxminded.university;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableJpaRepositories("ua/com/foxminded/university/repository")
 @EntityScan("ua/com/foxminded/university/model")
@@ -11,5 +16,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
